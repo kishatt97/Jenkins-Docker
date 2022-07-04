77c26d26da651099d098c4ba25f8ca96b9cf9249
Kishori Attarde <kishoria1697@gmail.com>
2022-07-04 16:36:56 +0530

    Java Program added

---
+++ src/main/java/com/app/DockerDemo1/TestMain.java
@@ -0,0 +1,15 @@
+package com.app.DockerDemo1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestMain {
+	
+	@Test
+	public void testInputIsEven(){
+	assertTrue(Main.checkIfInputIsAnEvenNumber(23)); 
+
+	//Assertion
+	}
+
+}
